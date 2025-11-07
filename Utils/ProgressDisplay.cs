c3acdc4c5b51ba48d09b590aee09856bc75b1c80
Asy0y0 <2646478736@qq.com>
2025-11-07 23:43:23 +0800

    优化了输出日志

--- Utils/ProgressDisplay.cs
+++ Utils/ProgressDisplay.cs
@@ -10,6 +10,8 @@ namespace IELTS_Learning_Tool.Utils
             string[] spinner = { "|", "/", "-", "\\" };
             int spinnerIndex = 0;
             string lastStatus = "";
+            int lastStep = -1;
+            bool isFirstUpdate = true;
 
             while (!progress.IsComplete)
             {
@@ -17,11 +19,17 @@ namespace IELTS_Learning_Tool.Utils
                 int currentStep = progress.CurrentStep;
 
                 // 只在状态改变时更新显示
-                if (currentStatus != lastStatus || currentStep != progress.LastDisplayedStep)
+                if (currentStatus != lastStatus || currentStep != lastStep)
                 {
-                    Console.Write("\r");
-                    Console.Write(new string(' ', Console.WindowWidth - 1));
-                    Console.Write("\r");
+                    // 如果不是第一次更新，保留之前的输出并换行
+                    if (!isFirstUpdate)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        isFirstUpdate = false;
+                    }
 
                     int percentage = progress.GetPercentage();
                     string spinnerChar = spinner[spinnerIndex % spinner.Length];
@@ -31,17 +39,29 @@ namespace IELTS_Learning_Tool.Utils
                     Console.ResetColor();
 
                     lastStatus = currentStatus;
+                    lastStep = currentStep;
                     progress.LastDisplayedStep = currentStep;
+                    spinnerIndex++;
+                }
+                else
+                {
+                    // 状态未改变时，更新旋转动画
+                    Console.Write("\r");
+                    int percentage = progress.GetPercentage();
+                    string spinnerChar = spinner[spinnerIndex % spinner.Length];
+                    
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write($"[{spinnerChar}] {currentStatus} ({percentage}%)");
+                    Console.ResetColor();
+                    
                     spinnerIndex++;
                 }
 
                 await Task.Delay(100);
             }
 
-            // 清除进度行
-            Console.Write("\r");
-            Console.Write(new string(' ', Console.WindowWidth - 1));
-            Console.Write("\r");
+            // 完成时，换行并保留最后的进度信息
+            Console.WriteLine();
         }
     }
 }
